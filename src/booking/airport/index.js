b9a206d9dd9e7a1d3fddb0a6c6b57c498eca84a2
Sushant Singh <sushantr134@gmail.com>
2019-08-15 18:27:14 +0530

    heading issue fix

--- src/booking/airport/index.js
+++ src/booking/airport/index.js
@@ -5,31 +5,39 @@ import { Input } from "../../common/Input";
 import styles from "./styles.module.scss";
 import { Grid } from "../../common/Grid";
 
+const FormContent = () => {
+  return (
+    <>
+      <div style={{ position: "relative" }}>
+        <span className={styles.FromIcon} />
+        <Input placeholder='From (airport, address)' type='text' Grouped={true} style={{ paddingLeft: "50px" }} />
+        <span className={styles.RouteIcon} />
+        <Input placeholder='To (airport, address)' type='text' Grouped={false} style={{ paddingLeft: "50px" }} />
+        <span className={styles.ToIcon} />
+      </div>
+
+      <label>Pickup time</label>
+      <Input placeholder='Pickup time' type='time' Grouped={false} />
+
+      <Grid col='2'>
+        <div>
+          <label>Passengers</label>
+          <Input placeholder='0' type='number' min={0} Grouped={false} />
+        </div>
+        <div>
+          <label>Luggage pieces</label>
+          <Input placeholder='0' type='number' Grouped={false} />
+        </div>
+      </Grid>
+    </>
+  );
+};
+
 export const AirportBookingWindow = () => {
   return (
     <CenterContainer>
-      <FormContainer heading={"Book a transfer"}>
-        <div style={{ position: "relative" }}>
-          <span className={styles.FromIcon} />
-          <Input placeholder='From (airport, address)' type='text' Grouped={true} style={{ paddingLeft: "50px" }} />
-          <span className={styles.RouteIcon} />
-          <Input placeholder='To (airport, address)' type='text' Grouped={false} style={{ paddingLeft: "50px" }} />
-          <span className={styles.ToIcon} />
-        </div>
-
-        <label>Pickup time</label>
-        <Input placeholder='Pickup time' type='time' Grouped={false} />
-
-        <Grid col='2'>
-          <div>
-            <label>Passengers</label>
-            <Input placeholder='0' type='number' min={0} Grouped={false} />
-          </div>
-          <div>
-            <label>Luggage pieces</label>
-            <Input placeholder='0' type='number' Grouped={false} />
-          </div>
-        </Grid>
+      <FormContainer heading={"Start Your Trip with Rydeu"}>
+        <FormContent />
       </FormContainer>
     </CenterContainer>
   );
--- src/containers/FormContainer/index.js
+++ src/containers/FormContainer/index.js
@@ -6,7 +6,7 @@ import { SuccessButton } from "../../common/Button/success";
 export const FormContainer = props => {
   return (
     <div className={styles.formContainer}>
-      <h1>{props.heading}</h1>
+      <h1 className={styles.heading}>{props.heading}</h1>
       <form>
         {props.children}
         <SuccessButton textName='Continue' onclick={() => alert("hello")} type='submit' />
